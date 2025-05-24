8f92a98c72907506fef2580e7ab44ebff723ee7c
jmt1423 <60321773+jmt1423@users.noreply.github.com>
2025-05-24 11:15:47 +0100

    bugfix: need steps in drag and drop test functionality for chrome and webkit

--- tests/e2e/builder.spec.ts
+++ tests/e2e/builder.spec.ts
@@ -1,23 +1,33 @@
 import { test, expect } from "@playwright/test";
 import type { Locator, Page } from "@playwright/test";
 
-const dragElement = async (page: Page, originElement: Locator, destinationElement: Locator) => {
+const dragElement = async (
+  page: Page,
+  originElement: Locator,
+  destinationElement: Locator,
+) => {
   await originElement.hover();
   await page.mouse.down();
   const box = (await destinationElement.boundingBox())!;
   await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
-  await page.mouse.move(box.x + box.width / 2, box.y - 30 + box.height / 2);
+  await page.mouse.move(box.x + box.width / 2, box.y - 30 + box.height / 2, {steps: 10});
   await page.mouse.up();
-}
+};
 
 test.describe("Test drag and drop functions", () => {
   test("Text input should be visible", async ({ page }) => {
     await page.goto("http://localhost:5173");
 
-    const originElement = page.getByRole("button", { name: "Text Single line text field" })
-    const destinationElement = page.getByTestId('drop-area').locator('div').filter({ hasText: 'Submit' }).nth(1)
+    const originElement = page.getByRole("button", {
+      name: "Text Single line text field",
+    });
+    const destinationElement = page
+      .getByTestId("drop-area")
+      .locator("div")
+      .filter({ hasText: "Submit" })
+      .nth(1);
 
-    await dragElement(page, originElement, destinationElement)
+    await dragElement(page, originElement, destinationElement);
 
     await expect(page.getByText("Client Name")).toBeVisible();
   });
@@ -25,14 +35,24 @@ test.describe("Test drag and drop functions", () => {
   test("Text and number should be visible", async ({ page }) => {
     await page.goto("http://localhost:5173");
     //
-    const o1 = page.getByRole('button', { name: 'Number Single number input' })
-    const d1 = page.getByTestId('drop-area').locator('div').filter({ hasText: 'Submit' }).nth(1)
-    await dragElement(page, o1, d1)
+    const o1 = page.getByRole("button", { name: "Number Single number input" });
+    const d1 = page
+      .getByTestId("drop-area")
+      .locator("div")
+      .filter({ hasText: "Submit" })
+      .nth(1);
+    await dragElement(page, o1, d1);
     await expect(page.getByText("Client Age")).toBeVisible();
 
-    const o2 = page.getByRole("button", { name: "Text Single line text field" })
-    const d2 = page.getByTestId('drop-area').locator('div').filter({ hasText: 'Client AgeThis is help text' }).nth(1)
-    await dragElement(page, o2, d2)
+    const o2 = page.getByRole("button", {
+      name: "Text Single line text field",
+    });
+    const d2 = page
+      .getByTestId("drop-area")
+      .locator("div")
+      .filter({ hasText: "Client AgeThis is help text" })
+      .nth(1);
+    await dragElement(page, o2, d2);
     await expect(page.getByText("Client Name")).toBeVisible();
-  })
-})
+  });
+});
